c634df35ffc5e6a17909debdf15444fe98e5702e
ebelestella4 <ebelestella4@gmail.com>
2022-09-15 19:33:20 -0700

    my first

---
+++ test2.py
@@ -0,0 +1,5 @@
+Abiola is making me to cook for him even if he know am in the house listenning to my class. after he will say am not paying attention in the class i wounder what this man wants from me.
+
+can some one tell him to get off my back. 
+
+i want to piece my nose. i want a nose ring but because he is my house i cannot go agian him. it not that i can not do it but does a nose ring worth breaking the peace in my home no is the answer. 
